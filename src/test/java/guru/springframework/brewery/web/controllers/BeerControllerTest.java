4356b19f3ff7394f543242658e283a36c11b6f2d
Molinari <matteomolinari93@gmail.com>
2020-10-19 13:28:34 +0200

    @WebMvc Test Slice

--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -3,6 +3,7 @@ package guru.springframework.brewery.web.controllers;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,26 +17,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import guru.springframework.brewery.services.BeerService;
 import guru.springframework.brewery.web.model.BeerDto;
@@ -43,16 +37,13 @@ import guru.springframework.brewery.web.model.BeerPagedList;
 import guru.springframework.brewery.web.model.BeerStyleEnum;
 
 
-
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(BeerController.class)
 public class BeerControllerTest {
 
-	@Mock
+	@MockBean
 	BeerService beerService;
 	
-	@InjectMocks
-	BeerController beerController;
-	
+	@Autowired
 	MockMvc mockMvc;
 	
 	BeerDto validBeer;
@@ -68,16 +59,16 @@ public class BeerControllerTest {
 				.createdDate(OffsetDateTime.now())
 				.lastModifiedDate(OffsetDateTime.now())
 				.build();
-		
-		mockMvc = MockMvcBuilders.standaloneSetup(beerController)
-				.setMessageConverters(jackson2HttpMessageConverter())
-				.build();
+	}
+	
+	@AfterEach
+	void tearDown() {
+		reset(beerService);
 	}
 	
 	@Test
 	void testGetBeerById() throws Exception {
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
-		
 		
 		when(beerService.findBeerById(any())).thenReturn(validBeer);
 		
@@ -134,17 +125,5 @@ public class BeerControllerTest {
 				.andExpect(jsonPath("$.content", hasSize(2)))
 				.andExpect(jsonPath("$.content[0].id", is(validBeer.getId().toString())));
 		}		
-	}
-	
-	public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-		objectMapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
-		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		
-		objectMapper.registerModule(new JavaTimeModule());
-		return new MappingJackson2HttpMessageConverter(objectMapper);
-	}
-	
-	
+	}	
 }
